9801d97619b6dbdff25f71799549feb800a76e60
Toh Yien <tohyien0017@gmail.com>
2025-02-05 16:32:48 +0800

    update

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,4 @@
-export const API_URL = "http://localhost:5555";
+export const API_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5555/api"
+    : "https://b13-yien23.mak3r.dev/api";
